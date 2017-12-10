cf134838df1a5648e4da2527cbceeb1228d6daa6
lu <hiluyunxiao@126.com>
2017-12-10 10:08:33 +0800

    前台大广告轮播图展示

--- e3mall-content/e3mall-content-interface/src/main/java/cn/e3mall/content/service/ContentService.java
+++ e3mall-content/e3mall-content-interface/src/main/java/cn/e3mall/content/service/ContentService.java
@@ -1,6 +1,9 @@
 package cn.e3mall.content.service;
 
+import java.util.List;
+
 import cn.e3mall.pojo.TbContent;
+import cn.e3mall.utils.ADItem;
 import cn.e3mall.utils.DataGridPageBean;
 import cn.e3mall.utils.E3mallResult;
 
@@ -11,4 +14,7 @@ public interface ContentService {
 
 	// 保存内容
 	E3mallResult saveContent(TbContent content);
+
+	// 根据分类id查询所有
+	List<ADItem> findAllByCategoryId(Long categoryId);
 }
--- e3mall-content/e3mall-content-service/src/main/java/cn/e3mall/content/service/imp/ContentServiceImpl.java
+++ e3mall-content/e3mall-content-service/src/main/java/cn/e3mall/content/service/imp/ContentServiceImpl.java
@@ -1,9 +1,11 @@
 package cn.e3mall.content.service.imp;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
@@ -13,6 +15,7 @@ import cn.e3mall.mapper.TbContentMapper;
 import cn.e3mall.pojo.TbContent;
 import cn.e3mall.pojo.TbContentExample;
 import cn.e3mall.pojo.TbContentExample.Criteria;
+import cn.e3mall.utils.ADItem;
 import cn.e3mall.utils.DataGridPageBean;
 import cn.e3mall.utils.E3mallResult;
 
@@ -47,6 +50,48 @@ public class ContentServiceImpl implements ContentService {
 		content.setUpdated(date);
 		contentMapper.insert(content);
 		return E3mallResult.ok();
+	}
+
+	@Value("${WIDTH}")
+	private Integer WIDTH;
+	@Value("${WIDTHB}")
+	private Integer WIDTHB;
+	@Value("${HEIGHT}")
+	private Integer HEIGHT;
+	@Value("${HEIGHTB}")
+	private Integer HEIGHTB;
+
+	@Override
+	public List<ADItem> findAllByCategoryId(Long categoryId) {
+		// 创建example对象
+		TbContentExample example = new TbContentExample();
+		// 设置查询参数
+		Criteria createCriteria = example.createCriteria();
+		createCriteria.andCategoryIdEqualTo(categoryId);
+		// 查询
+		List<TbContent> cList = contentMapper.selectByExample(example);
+		// 创建广告集合，封装首页分类数据
+		ArrayList<ADItem> list = new ArrayList<ADItem>();
+		// 循环，封装首页分类数据
+		for (TbContent content : cList) {
+			// 创建AdItem对象
+			ADItem adItem = new ADItem();
+			// 设置商品提示
+			adItem.setAlt(content.getTitle());
+			adItem.setHeight(HEIGHT);
+			adItem.setHeightB(HEIGHTB);
+			// 设置商品购买地址
+			adItem.setHref(content.getUrl());
+			// 设置首页商品图片地址
+			adItem.setSrc(content.getPic());
+			// 设置首页商品图片地址2
+			adItem.setSrcB(content.getPic2());
+			adItem.setWidth(WIDTH);
+			adItem.setWidthB(WIDTHB);
+			// 把ad放入adList集合
+			list.add(adItem);
+		}
+		return list;
 	}
 
 }
